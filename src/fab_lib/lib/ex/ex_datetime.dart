b45ad5594e923c9e4d133da63d786950cc5d4413
Adriano Sepe <adrianosepe@hotmail.com>
2021-01-25 23:36:42 -0300

    Added custom file format using DateTime

--- src/fab_lib/lib/ex/ex_datetime.dart
+++ src/fab_lib/lib/ex/ex_datetime.dart
@@ -1,24 +1,21 @@
 part of fab_lib;
 
 class ExDateTime {
-  static final intl.DateFormat defFormatSmallWithoutSeconds =
-      intl.DateFormat('dd/MM/yy HH:mm');
-  static final intl.DateFormat defFormatWithoutSeconds =
-      intl.DateFormat('dd/MM/yyyy HH:mm');
-  static final intl.DateFormat defFormat =
-      intl.DateFormat('dd/MM/yyyy HH:mm:ss');
+  static final intl.DateFormat defFileNameFormat = intl.DateFormat('ddMMyyHHmm');
+  static final intl.DateFormat defFormatSmallWithoutSeconds = intl.DateFormat('dd/MM/yy HH:mm');
+  static final intl.DateFormat defFormatWithoutSeconds = intl.DateFormat('dd/MM/yyyy HH:mm');
+  static final intl.DateFormat defFormat = intl.DateFormat('dd/MM/yyyy HH:mm:ss');
   static final intl.DateFormat defFormatAsDate = intl.DateFormat('dd/MM/yyyy');
 
   static String format(DateTime value) => defFormat.format(value);
+  static String formatFileName(DateTime value) => defFileNameFormat.format(value);
   static String formatAsDate(DateTime value) => defFormatAsDate.format(value);
-  static String formatSmallWithoutSeconds(DateTime value) =>
-      defFormatSmallWithoutSeconds.format(value);
-  static String formatWithoutSeconds(DateTime value) =>
-      defFormatWithoutSeconds.format(value);
+  static String formatSmallWithoutSeconds(DateTime value) => defFormatSmallWithoutSeconds.format(value);
+  static String formatWithoutSeconds(DateTime value) => defFormatWithoutSeconds.format(value);
 
   static DateTime parse(String value, {bool utc = false}) {
     try {
-      if (value == null) {
+      if (ExString.isNullOrEmpty(value)) {
         return null;
       }
 
@@ -26,7 +23,11 @@ class ExDateTime {
 
       return utc ? dt.toUtc() : dt.toLocal();
     } catch (Exception) {
-      return defFormat.parse(value, utc);
+      try {
+        return defFormat.parse(value, utc);
+      } catch (Exception) {
+        return defFormatAsDate.parse(value, utc);
+      }
     }
   }
 
@@ -47,13 +48,11 @@ class ExDateTime {
 
   static DateTime date(DateTime n) => DateTime(n.year, n.month, n.day);
 
-  static DateTime getDate(int timestamp, {bool isUtc = false}) =>
-      DateTime.fromMicrosecondsSinceEpoch(timestamp, isUtc: isUtc);
+  static DateTime getDate(int timestamp, {bool isUtc = false}) => DateTime.fromMicrosecondsSinceEpoch(timestamp, isUtc: isUtc);
 
   static DateRange adjustTime(DateRange range) {
     final begin = ExDateTime.date(range.begin);
-    var end =
-        ExDateTime.date(range.end).add(Duration(seconds: (24 * 3600) - 1));
+    var end = ExDateTime.date(range.end).add(Duration(seconds: (24 * 3600) - 1));
 
     return DateRange(begin, end);
   }
